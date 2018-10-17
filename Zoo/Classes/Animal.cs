addaec3e528c638b6f919c1ab778657eeb927cf5
suzanne su <suhysu@gmail.com>
2018-10-16 19:19:30 -0700

    added 3 class and interface

---
+++ Zoo/Classes/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Classes
+{
+    class Animal
+    {
+        public virtual void eat()
+        {
+
+        }
+        public virtual void sound()
+        {
+
+        }
+        public virtual void sleep()
+        {
+
+        }
+
+        
+    }
+}
