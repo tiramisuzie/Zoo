cd6f9fc1d324abdcab833db31516a25d364bc730
suzanne su <suhysu@gmail.com>
2018-10-24 00:46:28 -0700

    add Demigod

---
+++ Zoo/Abstract/DemiGod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Abstract
+{
+    public abstract class DemiGod
+    {
+        public abstract int age { get; set; }
+
+        public abstract string SuperPower();
+    }
+}
