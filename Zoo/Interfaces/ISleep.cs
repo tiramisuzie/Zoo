190ed869c5bf5c92e5a4428d394abc2d7aed29c8
suzanne su <suhysu@gmail.com>
2018-10-24 00:47:23 -0700

    created interface

---
+++ Zoo/Interfaces/ISleep.cs
@@ -0,0 +1,16 @@
+﻿namespace Zoo.Interfaces
+{
+    public interface ISleep
+    {
+        /// <summary>
+        /// property
+        /// </summary>
+        bool Asleep { get; set; }
+
+        /// <summary>
+        /// method
+        /// </summary>
+        /// <returns>string</returns>
+        string Nap();
+    }
+}
